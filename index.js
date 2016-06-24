cdd12100ff79cf1836e7f7544664cf068ce93fd3
Daniel Trojer <dtrojer.mmt-b2014@fh-salzburg.ac.at>
2016-06-24 15:54:40 +0200

    add upload to project

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const app = express();
 const livereload = require('livereload');
 const server = livereload.createServer();
 const exec = require('child_process').exec;
+const multer  =   require('multer');
+
+var storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
+var upload = multer({ storage : storage}).single('photo');
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -24,6 +35,21 @@ app.get('/q/:id', function(req, res, next) {
 		res.send(stdout);
 	});
 })
+
+app.post('/upload',function(req,res){
+    upload(req,res,function(err) {
+    	
+        if(err) {
+
+            return res.end("Error uploading file.");
+        }
+        console.log(res.req.file.path) // IS PATH OF UPLOADED IMAGE
+
+        res.end("File is uploaded");
+    });
+});
+
+
 
 app.listen(3000, function () {
   console.log('Listening on :3000');
--- public/index.js
+++ public/index.js
@@ -1,61 +1,72 @@
 'use strict'
-
-var source = $("#some-template").html(); 
+var source = $("#some-template").html();
 var template = Handlebars.compile(source);
-
-Handlebars.registerHelper('img', function(name){
-  const width = Math.max(Math.floor(Math.random()*500),100);
-  const height = Math.max(Math.floor(Math.random()*500),100);
-  const html = `<img src="${name}">`;
-  return new Handlebars.SafeString(html);
+Handlebars.registerHelper('img', function(name) {
+    const width = Math.max(Math.floor(Math.random() * 500), 100);
+    const height = Math.max(Math.floor(Math.random() * 500), 100);
+    const html = `<img src="${name}">`;
+    return new Handlebars.SafeString(html);
 });
-
-$('.button-group button').click(function () {
-  search($('#search').val());
-});
-
-$('#search').keyup(function(e) {
-  if(e.which === 13) {
+$('.button-group button').click(function() {
     search($('#search').val());
-  }
 });
-
+$('#search').keyup(function(e) {
+    if (e.which === 13) {
+        search($('#search').val());
+    }
+});
 
 function search(query) {
-  // SEARCH INVOCATION CODE GOES IN HERE
-  //http://www.cp.jku.at/misc/div-2014/devset/img/acropolis_athens/2951950136.jpg
-  // GET JSON FROM /q/:query
-  $.ajax({
-    url: `http://localhost:3000/q/${query}`,
-    success: function(res){
-      renderResult(parse(res));
-      transitionToResult();
+    // SEARCH INVOCATION CODE GOES IN HERE
+    //http://www.cp.jku.at/misc/div-2014/devset/img/acropolis_athens/2951950136.jpg
+    // GET JSON FROM /q/:query
+    $.ajax({
+        url: `http://localhost:3000/q/${query}`,
+        success: function(res) {
+            renderResult(parse(res));
+            transitionToResult();
+        }
+    });
+}
+
+function parse(res) {
+    res = res.replace(/\[/g, '');
+    res = res.replace(/\]/g, '');
+    res = res.replace(/\"/g, '');
+    res = res.split(",")
+    let buff = []
+    for (let index in res) {
+        if (res[index] != "null") buff.push({
+            url_b: res[index]
+        })
     }
-  });
-
-  
+    return {
+        docs: buff
+    }
 }
 
-function parse(res){
-  res = res.replace(/\[/g,'');
-  res = res.replace(/\]/g,'');
-  res = res.replace(/\"/g,'');
-  res = res.split(",")
-  let buff = []
-  for(let index in res){
-    if(res[index] != "null")
-      buff.push({url_b: res[index]})
-  }
-  return {docs: buff}
-}
-
-function renderResult(data){
-  $('#wrapper').html('');
-  $('#wrapper').append(template(data));
+function renderResult(data) {
+    $('#wrapper').html('');
+    $('#wrapper').append(template(data));
 }
 
 function transitionToResult() {
-  $('header').removeClass('column');
-  $('header').addClass('result-mode');
-  $('#filters').addClass('show');
+    $('header').removeClass('column');
+    $('header').addClass('result-mode');
+    $('#filters').addClass('show');
 }
+
+$('#uploadForm').on('submit', function() {
+    $(this).ajaxSubmit({
+        error: function(xhr) {
+            status('Error: ' + xhr.status);
+        },
+        success: function(response) {
+            console.log("success");
+            console.log(response)
+        }
+    });
+    //Very important line, it disable the page refresh.
+    return false;
+});
+// Catch the form submit and upload the files
